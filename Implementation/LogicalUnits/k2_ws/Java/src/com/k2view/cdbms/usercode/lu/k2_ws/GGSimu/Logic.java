ed3dd500b2903f11a998cee9bcbe71a7c67eed5b
nirkehat <nirkehat@k2view.com>
2019-01-08 13:08:18 -0600

    _

--- Implementation/LogicalUnits/k2_ws/Java/src/com/k2view/cdbms/usercode/lu/k2_ws/GGSimu/Logic.java
+++ Implementation/LogicalUnits/k2_ws/Java/src/com/k2view/cdbms/usercode/lu/k2_ws/GGSimu/Logic.java
@@ -8,6 +8,7 @@ import java.util.*;
 import java.sql.*;
 import java.math.*;
 import java.io.*;
+import java.util.prefs.Preferences;
 
 import com.k2view.cdbms.shared.*;
 import com.k2view.cdbms.shared.user.WebServiceUserCode;
@@ -18,7 +19,6 @@ import com.k2view.cdbms.shared.logging.LogEntry.*;
 import com.k2view.cdbms.func.oracle.OracleToDate;
 import com.k2view.cdbms.func.oracle.OracleRownum;
 import com.k2view.cdbms.usercode.lu.k2_ws.*;
-
 import static com.k2view.cdbms.shared.utils.UserCodeDescribe.FunctionType.*;
 import static com.k2view.cdbms.shared.user.ProductFunctions.*;
 import static com.k2view.cdbms.usercode.common.SharedLogic.*;
@@ -29,9 +29,15 @@ public class Logic extends WebServiceUserCode {
 
 
 	@out(name = "rs", type = String.class, desc = "")
-	public static String wsUpdateGGSimu(String GGFullPath, String Stmt) throws Exception {
-		PrintWriter printWriter = printWriter = new PrintWriter(new FileOutputStream(new File(GGFullPath)));
-		printWriter.println(Stmt);
+	public static String wsUpdateGGSimu(@desc("IF Diffrent then default $k2_home/GGSimulator") String GGSIMU_FILE_PATH, String GGSimuFileName, String Stmt, String params) throws Exception {
+		String GGLocation = "";
+		if(GGSIMU_FILE_PATH != null && !GGSIMU_FILE_PATH.equals("")){
+			GGLocation = GGSIMU_FILE_PATH;
+		}else{
+			GGLocation = System.getenv("K2_HOME") + "/GGSimulator/";
+		}
+		PrintWriter printWriter = printWriter = new PrintWriter(new FileOutputStream(new File(GGLocation + GGSimuFileName)));
+		printWriter.println(Stmt + ";" + params);
 		printWriter.close();
 		printWriter.flush();
 		return "Done";
