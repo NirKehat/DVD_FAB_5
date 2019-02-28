22d5736c31820dbd6785b834013b74a7ccf2f204
nirkehat <nirkehat@k2view.com>
2019-02-28 10:15:09 -0600

    Globals

---
+++ Implementation/LogicalUnits/k2_ws/Java/src/com/k2view/cdbms/usercode/lu/k2_ws/GLOBALS/Logic.java
@@ -0,0 +1,42 @@
+/////////////////////////////////////////////////////////////////////////
+// Project Web Services
+/////////////////////////////////////////////////////////////////////////
+
+package com.k2view.cdbms.usercode.lu.k2_ws.GLOBALS;
+
+import java.util.*;
+import java.sql.*;
+import java.math.*;
+import java.io.*;
+
+import com.k2view.cdbms.shared.*;
+import com.k2view.cdbms.shared.user.WebServiceUserCode;
+import com.k2view.cdbms.sync.*;
+import com.k2view.cdbms.lut.*;
+import com.k2view.cdbms.shared.utils.UserCodeDescribe.*;
+import com.k2view.cdbms.shared.logging.LogEntry.*;
+import com.k2view.cdbms.func.oracle.OracleToDate;
+import com.k2view.cdbms.func.oracle.OracleRownum;
+import com.k2view.cdbms.usercode.lu.k2_ws.*;
+
+import static com.k2view.cdbms.shared.utils.UserCodeDescribe.FunctionType.*;
+import static com.k2view.cdbms.shared.user.ProductFunctions.*;
+import static com.k2view.cdbms.usercode.common.SharedLogic.*;
+import static com.k2view.cdbms.usercode.common.SharedGlobals.*;
+
+@SuppressWarnings({"unused", "DefaultAnnotationParam"})
+public class Logic extends WebServiceUserCode {
+
+
+	@out(name = "rs", type = String.class, desc = "")
+	public static String wsSetGlobalValueCrossNodes(String LU_NAME, String GLOBAL_NAME, String GLOBAL_VALUE) throws Exception {
+		GlobalSettingsDaoFactory.getDao().updateSetting(GlobalSettingsDao.GlobalSettingName.GLOBAL, LU_NAME, GLOBAL_NAME, GLOBAL_VALUE);
+		GlobalSettingsDaoFactory.getDao().getSettings(GlobalSettingsDao.GlobalSettingName.GLOBAL, LU_NAME).get(GLOBAL_NAME);
+		return "SET IS DONE";
+	}
+
+	
+	
+
+	
+}
