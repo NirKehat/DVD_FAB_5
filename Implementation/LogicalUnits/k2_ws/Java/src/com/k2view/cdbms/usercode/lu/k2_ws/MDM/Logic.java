f2c69a966cee9c0b801fca0f27b0e2c2d83a6d9b
nirkehat <nirkehat@k2view.com>
2019-07-22 10:17:13 -0500

    _

--- Implementation/LogicalUnits/k2_ws/Java/src/com/k2view/cdbms/usercode/lu/k2_ws/MDM/Logic.java
+++ Implementation/LogicalUnits/k2_ws/Java/src/com/k2view/cdbms/usercode/lu/k2_ws/MDM/Logic.java
@@ -31,108 +31,109 @@ import static com.k2view.cdbms.usercode.common.SharedGlobals.*;
 @SuppressWarnings({"unused", "DefaultAnnotationParam"})
 public class Logic extends WebServiceUserCode {
 
-    @out(name = "res", type = Object.class, desc = "")
-    public static Object get(String arg) throws Exception {
-        Map<String, Object> rs = new LinkedHashMap<>();
-        Map<String, Object> queryMapRs = new LinkedHashMap<>();
-        List<Map<String, Object>> listRs = new LinkedList<>();
-        HttpServletResponse wsResp = response();
-        HttpServletRequest wsReq = request();
-        wsReq.getReader();
-        Map<String, String[]> requestParamsMap = requestParams();
-        ServletOutputStream sos = null;
-        if (requestParamsMap.get("format") == null) sos = wsResp.getOutputStream();
-        ResultSet queryRS = null;
-        PreparedStatement preStmt = null;
-
-        try {
-            String fields = null;
-            if (requestParamsMap.get("fields") != null) {
-                fields = requestParamsMap.get("fields")[0];
-            } else {
-                fields = "*";
-            }
-
-            String[] args = requestParamsMap.get("arg")[0].split("/");
-            String luName = args[0];
-            String iid = args[1];
-            String tableName = args[2];
-
-            StringBuilder sqlStmt = new StringBuilder().append("Select " + fields + " from " + tableName);
-
-            String prefix = "";
-            String prefixHref = "?";
-            StringBuilder hrefFilters = new StringBuilder();
-            Set<Object> preStmtVals = new LinkedHashSet<>();
-            for (Map.Entry<String, String[]> vals : requestParamsMap.entrySet()) {
-                if (!vals.getKey().equals("token") && !vals.getKey().equals("arg")) {
-                    hrefFilters.append(prefixHref + vals.getKey() + "=" + vals.getValue()[0]);
-                    prefixHref = "&";
-                }
-                if (vals.getKey().equals("token") || vals.getKey().equals("arg") || vals.getKey().equals("fields") || vals.getKey().equals("format"))
-                    continue;
-                if (prefix.equals("")) sqlStmt.append(" Where ");
-                sqlStmt.append(prefix + vals.getKey() + " = ?");
-                preStmtVals.add(vals.getValue()[0]);
-                prefix = " and ";
-            }
-
-            Connection fabCon = getConnection("fabric");
-            fabCon.createStatement().execute("set sync off");
-            fabCon.createStatement().execute("get " + luName + "." + iid);
-
-            if (sos == null) {
-                return ludb(luName, iid).fetch(sqlStmt.toString(), Arrays.copyOf(preStmtVals.toArray(), preStmtVals.size(), Object[].class));
-            } else {
-                preStmt = fabCon.prepareStatement(sqlStmt.toString());
-                int cntPreVals = 1;
-                for (Object preStmtVal : preStmtVals) {
-                    preStmt.setObject(cntPreVals, preStmtVal);
-                    cntPreVals++;
-                }
-
-                queryRS = preStmt.executeQuery();
-                ResultSetMetaData queryRSMD = queryRS.getMetaData();
-                while (queryRS.next() && sos != null) {
-                    for (int i = 1; i < queryRSMD.getColumnCount() + 1; i++) {
-                        queryMapRs.put(queryRSMD.getColumnName(i), queryRS.getObject(i));
-                    }
-                    listRs.add(queryMapRs);
-                }
-                rs.put("id", iid);
-                rs.put("href", getRequestHeaders().get("host") + "/GET/" + requestParamsMap.get("arg")[0] + hrefFilters);
-                rs.put(tableName, listRs);
-
-                if (queryRS != null && listRs.size() == 0) {
-                    wsResp.setStatus(204);
-                } else if (sos != null && listRs.size() != 0) {
-                    sos.println(rs.toString());
-                }
-
-            }
-
-        } catch (Exception e) {
-            if (e.getMessage().contains("Access to")) {
-                if (sos != null) sos.println(e.getMessage());
-                wsResp.setStatus(401);
-            } else {
-                if (sos != null) sos.println("Bad Request!");
-                wsResp.setStatus(400);
-            }
-            if (sos == null) {
-                throw e;
-            } else {
-                log.error("get Exception", e);
-            }
-        } finally {
-            if (preStmt != null) preStmt.close();
-            if (queryRS != null) queryRS.close();
-            if (sos != null) {
-                sos.flush();
-                sos.close();
-            }
-        }
-        return null;
-    }
+	@out(name = "res", type = Object.class, desc = "")
+	public static Object get(String arg) throws Exception {
+		Map<String, Object> rs = new LinkedHashMap<>();
+		Map<String, Object> queryMapRs = new LinkedHashMap<>();
+		List<Map<String, Object>> listRs = new LinkedList<>();
+		HttpServletResponse wsResp = response();
+		HttpServletRequest wsReq = request();
+		wsReq.setAttribute(com.k2view.cdbms.ws.WebServiceServlet.SUPPRESS_REPORT, true);
+		wsReq.getReader();
+		Map<String, String[]> requestParamsMap = requestParams();
+		ServletOutputStream sos = null;
+		if (requestParamsMap.get("format") == null) sos = wsResp.getOutputStream();
+		ResultSet queryRS = null;
+		PreparedStatement preStmt = null;
+		
+		try {
+		    String fields = null;
+		    if (requestParamsMap.get("fields") != null) {
+		        fields = requestParamsMap.get("fields")[0];
+		    } else {
+		        fields = "*";
+		    }
+		
+		    String[] args = requestParamsMap.get("arg")[0].split("/");
+		    String luName = args[0];
+		    String iid = args[1];
+		    String tableName = args[2];
+		
+		    StringBuilder sqlStmt = new StringBuilder().append("Select " + fields + " from " + tableName);
+		
+		    String prefix = "";
+		    String prefixHref = "?";
+		    StringBuilder hrefFilters = new StringBuilder();
+		    Set<Object> preStmtVals = new LinkedHashSet<>();
+		    for (Map.Entry<String, String[]> vals : requestParamsMap.entrySet()) {
+		        if (!vals.getKey().equals("token") && !vals.getKey().equals("arg")) {
+		            hrefFilters.append(prefixHref + vals.getKey() + "=" + vals.getValue()[0]);
+		            prefixHref = "&";
+		        }
+		        if (vals.getKey().equals("token") || vals.getKey().equals("arg") || vals.getKey().equals("fields") || vals.getKey().equals("format"))
+		            continue;
+		        if (prefix.equals("")) sqlStmt.append(" Where ");
+		        sqlStmt.append(prefix + vals.getKey() + " = ?");
+		        preStmtVals.add(vals.getValue()[0]);
+		        prefix = " and ";
+		    }
+		
+		    Connection fabCon = getConnection("fabric");
+		    fabCon.createStatement().execute("set sync off");
+		    fabCon.createStatement().execute("get " + luName + "." + iid);
+		
+		    if (sos == null) {
+		        return ludb(luName, iid).fetch(sqlStmt.toString(), Arrays.copyOf(preStmtVals.toArray(), preStmtVals.size(), Object[].class));
+		    } else {
+		        preStmt = fabCon.prepareStatement(sqlStmt.toString());
+		        int cntPreVals = 1;
+		        for (Object preStmtVal : preStmtVals) {
+		            preStmt.setObject(cntPreVals, preStmtVal);
+		            cntPreVals++;
+		        }
+		
+		        queryRS = preStmt.executeQuery();
+		        ResultSetMetaData queryRSMD = queryRS.getMetaData();
+		        while (queryRS.next() && sos != null) {
+		            for (int i = 1; i < queryRSMD.getColumnCount() + 1; i++) {
+		                queryMapRs.put(queryRSMD.getColumnName(i), queryRS.getObject(i));
+		            }
+		            listRs.add(queryMapRs);
+		        }
+		        rs.put("id", iid);
+		        rs.put("href", getRequestHeaders().get("host") + "/GET/" + requestParamsMap.get("arg")[0] + hrefFilters);
+		        rs.put(tableName, listRs);
+		
+		        if (queryRS != null && listRs.size() == 0) {
+		            wsResp.setStatus(204);
+		        } else if (sos != null && listRs.size() != 0) {
+		            sos.println(rs.toString());
+		        }
+		
+		    }
+		
+		} catch (Exception e) {
+		    if (e.getMessage().contains("Access to")) {
+		        if (sos != null) sos.println(e.getMessage());
+		        wsResp.setStatus(401);
+		    } else {
+		        if (sos != null) sos.println("Bad Request!");
+		        wsResp.setStatus(400);
+		    }
+		    if (sos == null) {
+		        throw e;
+		    } else {
+		        log.error("get Exception", e);
+		    }
+		} finally {
+		    if (preStmt != null) preStmt.close();
+		    if (queryRS != null) queryRS.close();
+		    if (sos != null) {
+		        sos.flush();
+		        sos.close();
+		    }
+		}
+		return null;
+	}
 
 }
